a24f5d5f030cc8b02b003ff4f34b3828c480c7e8
Ahsan Habib <ahsankshmiri@gmail.com>
2025-12-10 12:43:27 +0500

    ))

--- src/app/[locale]/(dashboard)/dashboard/page.tsx
+++ src/app/[locale]/(dashboard)/dashboard/page.tsx
@@ -1,9 +1,7 @@
 import { getTranslations } from "next-intl/server";
 import DashboardHeader from "@/components/dashboard/DashboardHeader";
 import DocumentsSection from "@/components/dashboard/DocumentsSection";
-import JobOpenings from "@/components/dashboard/JobOpenings";
-import CareerMap from "@/components/dashboard/CareerMap";
-import Interviews from "@/components/dashboard/Interviews";
+
 
 export default async function DashboardPage() {
     // Fetch translations on the server
@@ -17,12 +15,7 @@ export default async function DashboardPage() {
             {/* Documents Section */}
             <DocumentsSection />
 
-            {/* Bottom Grid */}
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                <JobOpenings />
-                <CareerMap />
-                <Interviews />
-            </div>
+
         </div>
     );
 }
